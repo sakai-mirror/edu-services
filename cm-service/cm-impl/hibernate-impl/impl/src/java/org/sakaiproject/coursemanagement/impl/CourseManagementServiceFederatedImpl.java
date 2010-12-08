3c52644f6d23ccccbf65341eed350fa371d02241
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-12-08 08:14:58 +0000

    dhorwitz@david-horwitz-4:~/branchManagement/edu-services-1.1.x> svn log -r83753 https://source.sakaiproject.org/svn/edu-services/trunk ------------------------------------------------------------------------ r83753 | david.horwitz@uct.ac.za | 2010-10-26 12:51:54 +0200 (Tue, 26 Oct 2010) | 2 lines
    SAK-19028 add new method to CM 
    
    ------------------------------------------------------------------------
    dhorwitz@david-horwitz-4:~/branchManagement/edu-services-1.1.x> svn
    merge -c83753 https://source.sakaiproject.org/svn/edu-services/trunk
    --- Merging r83753 into '.':
    U
    cm-service/cm-api/api/src/java/org/sakaiproject/coursemanagement/api/CourseManagementService.java
    U
    cm-service/cm-impl/hibernate-impl/hibernate/src/hibernate/org/sakaiproject/coursemanagement/impl/CourseOfferingCmImpl.hbm.xml
    U
    cm-service/cm-impl/hibernate-impl/impl/src/test/org/sakaiproject/coursemanagement/test/HibernateTestDataLoader.java
    U
    cm-service/cm-impl/hibernate-impl/impl/src/test/org/sakaiproject/coursemanagement/test/CourseManagementServiceTest.java
    U
    cm-service/cm-impl/hibernate-impl/impl/src/java/org/sakaiproject/coursemanagement/impl/CourseManagementServiceHibernateImpl.java
    U
    cm-service/cm-impl/hibernate-impl/impl/src/java/org/sakaiproject/coursemanagement/impl/CourseManagementServiceFederatedImpl.java
    U
    cm-service/cm-impl/hibernate-impl/impl/src/java/org/sakaiproject/coursemanagement/impl/CourseManagementServiceSampleChainImpl.java
    dhorwitz@david-horwitz-4:~/branchManagement/edu-services-1.1.x> svn
    merge -c83759 https://source.sakaiproject.org/svn/edu-services/trunk
    --- Merging r83759 into '.':
    G
    cm-service/cm-impl/hibernate-impl/impl/src/java/org/sakaiproject/coursemanagement/impl/CourseManagementServiceHibernateImpl.java
    G
    cm-service/cm-impl/hibernate-impl/impl/src/java/org/sakaiproject/coursemanagement/impl/CourseManagementServiceFederatedImpl.ja
    
    
    git-svn-id: https://source.sakaiproject.org/svn/edu-services/branches/edu-services-1.1.x@86133 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- cm-service/cm-impl/hibernate-impl/impl/src/java/org/sakaiproject/coursemanagement/impl/CourseManagementServiceFederatedImpl.java
+++ cm-service/cm-impl/hibernate-impl/impl/src/java/org/sakaiproject/coursemanagement/impl/CourseManagementServiceFederatedImpl.java
@@ -946,4 +946,17 @@ public class CourseManagementServiceFederatedImpl implements
 		}
 		return statusMap;
 	}
+
+	public List<CourseOffering> findActiveCourseOfferingsInCanonicalCourse(	String eid) {
+		log.debug("findActiveCourseOfferingsInCanonicalCourse(" + eid +")");
+		List<CourseOffering> ret = new ArrayList<CourseOffering>();
+		for(Iterator<CourseManagementService> implIter = implList.iterator(); implIter.hasNext();) {
+			CourseManagementService cm = implIter.next();
+			List<CourseOffering> col = cm.findActiveCourseOfferingsInCanonicalCourse(eid);
+			if (col != null) {
+				ret.addAll(col);
+			}
+		}
+		return ret;
+	}
 }
