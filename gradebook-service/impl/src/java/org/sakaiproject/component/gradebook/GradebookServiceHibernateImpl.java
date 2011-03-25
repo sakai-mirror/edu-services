c7cea73ac59a46c7d02f848a5a6965ba354cb339
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2011-03-25 15:05:34 +0000

    SAK-20335 merge trunk r90449
    git-svn-id: https://source.sakaiproject.org/svn/edu-services/branches/edu-services-1.1.x@90589 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/GradebookServiceHibernateImpl.java
+++ gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/GradebookServiceHibernateImpl.java
@@ -597,7 +597,7 @@ public class GradebookServiceHibernateImpl extends BaseHibernateManager implemen
 				while(it.hasNext()) {
 					cat_cat = (Category) it.next();
 					if(cat_tempt.getName().equals(cat_cat.getName())) {
-						catList.remove(cat_cat);
+						it.remove();
 						it = catList.iterator();
 					}	
 				}
